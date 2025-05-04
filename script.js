0671a27dd552f71d3de31e9ebbf5d1394bc3e545
neducod <nedunony16@gmail.com>
2025-05-04 17:59:21 +0100

    Responsive design for small screens

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ gsap.from(".box", {
     x: -500,             // Start position (move from left)
     ease: "power2.out",  // Easing function for smoothness
   });  
+
+
+/*POP UP CODE
+  window.onload = function () {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+
+    // Hide after 5 seconds (5000 milliseconds)
+    setTimeout(() => {
+      popup.style.display = 'none';
+    }, 5000);
+  };*/
